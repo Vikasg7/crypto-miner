0d4754eaf81181a85ece03d764b94490ae44950a
Vikas <v33vikasgautam@gmail.com>
2021-06-11 02:01:59 +0530

    Fixed indentation in index.js

--- src/index.js
+++ src/index.js
@@ -4,41 +4,41 @@ const { log } = require("console")
 
 const args =
    yargs
-    .usage("npm start -- [options]")
-    .options({
-       "a": {
-          type: "string",
-          demandOption: true,
-          describe: "host:port",
-          alias: "address"
-       },
-       "u": {
-          type: "string",
-          demandOption: true,
-          describe: "username",
-          alias: "user"
-       },
-       "p": {
-          type: "string",
-          demandOption: true,
-          describe: "password",
-          alias: "pass"
-       },
-       "w": {
-          type: "string",
-          demandOption: true,
-          describe: "miner's wallet",
-          alias: "wallet"
-       },
-       "t": {
-          type: "number",
-          demandOption: true,
-          describe: "threads",
-          alias: "threads",
-          default: 1
-       }
-    })
-    .help()
-    .argv
+      .usage("npm start -- [options]")
+      .options({
+         "a": {
+            type: "string",
+            demandOption: true,
+            describe: "host:port",
+            alias: "address"
+         },
+         "u": {
+            type: "string",
+            demandOption: true,
+            describe: "username",
+            alias: "user"
+         },
+         "p": {
+            type: "string",
+            demandOption: true,
+            describe: "password",
+            alias: "pass"
+         },
+         "w": {
+            type: "string",
+            demandOption: true,
+            describe: "miner's wallet",
+            alias: "wallet"
+         },
+         "t": {
+            type: "number",
+            demandOption: true,
+            describe: "threads",
+            alias: "threads",
+            default: 1
+         }
+      })
+      .help()
+      .argv
 
 main(args).subscribe(null, log)
